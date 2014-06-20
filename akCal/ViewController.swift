4e97bfe37b1000a8b7bbdc85073d4edb9ee77f26
yezhi <yezhi@yezhiMacBook.local>
2014-06-20 22:45:57 +0800

    Initial Commit

---
+++ akCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  akCal
+//
+//  Created by yezhi on 14-6-20.
+//  Copyright (c) 2014年 ak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
